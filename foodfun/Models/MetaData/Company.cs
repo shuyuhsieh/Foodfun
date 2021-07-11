9a338d5c8808ef64511f8922ef94fe9301240136
grace.tsai <gracetsai.3636@gmail.com>
2021-07-11 09:03:54 +0800

    companyMetaData

---
+++ foodfun/Models/MetaData/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace foodfun.Models
+{
+    [MetadataType(typeof(CompanyMetaData))]
+    public partial class Company
+    {
+        
+        public partial class CompanyMetaData
+        {
+            public int rowid { get; set; }
+            public Nullable<int> company_id { get; set; }
+            public string brandname { get; set; }
+            public string tel { get; set; }
+            public string fax { get; set; }
+            public string address { get; set; }
+            public Nullable<System.TimeSpan> opentime { get; set; }
+            public Nullable<System.TimeSpan> closetime { get; set; }
+            public string public_holiday { get; set; }
+            public string logoimage_path { get; set; }
+            public string indeximage_path { get; set; }
+        }
+    }
+}
