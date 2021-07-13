36c6e4d89389d6ad48bb358bee99218efc19bc71
miwa <miwa.huang0625@gmail.com>
2021-07-13 23:10:57 +0800

    Add Admin info and password change

--- foodfun/Areas/Admin/Controllers/AdminController.cs
+++ foodfun/Areas/Admin/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using foodfun.Models;
 
 namespace foodfun.Areas.Admin.Controllers
 {
@@ -14,5 +15,71 @@ namespace foodfun.Areas.Admin.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        [LoginAuthorize(RoleList = "Admin,Staff")]
+        public ActionResult UserInfo()
+        {
+            using (GoPASTAEntities db = new GoPASTAEntities())
+            {
+                var model = db.Users.Where(m => m.account_name == UserAccount.UserNo).FirstOrDefault();
+                return View(model);
+            }
+
+        }
+
+        [HttpPost]
+        [LoginAuthorize(RoleList = "Admin,Staff")]
+        public ActionResult UserInfo(Users model)
+        {
+
+            if (!ModelState.IsValid) return View(model);
+
+            using (GoPASTAEntities db = new GoPASTAEntities())
+            {
+                Users user = db.Users.Where(m => m.mno == model.mno).FirstOrDefault();
+                user.mname = model.mname;
+                user.birthday = model.birthday;
+                user.email = model.email;
+                user.phone = model.phone;
+                user.address = model.address;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ResetPassword()
+        {
+            using (GoPASTAEntities db = new GoPASTAEntities())
+            {
+                ResetPasswordViewModel model = new ResetPasswordViewModel();
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ResetPassword(ResetPasswordViewModel model)
+        {
+
+            if (!ModelState.IsValid) return View(model);
+
+            using (GoPASTAEntities db = new GoPASTAEntities())
+            {
+                Users user = db.Users.Where(m => m.account_name == UserAccount.UserNo).FirstOrDefault();
+                if (user != null)
+                {
+                    user.password = model.NewPassword;
+                    db.SaveChanges();
+
+                }
+                TempData["HeaderText"] = "密碼變更完成";
+                TempData["MessageText"] = "密碼已變更，下次請使用新密碼登入!!";
+                return RedirectToAction("MessageText");
+            }
+        }
+
     }
 }
