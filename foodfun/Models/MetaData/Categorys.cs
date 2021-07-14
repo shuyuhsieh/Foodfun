626997fdc6295a03bec9804403c705038b620565
Doris Hsieh <shuyu.doris@gmail.com>
2021-07-14 20:50:57 +0800

    adjust detail

--- foodfun/Models/MetaData/Categorys.cs
+++ foodfun/Models/MetaData/Categorys.cs
@@ -17,9 +17,9 @@ namespace foodfun.Models
             public int rowid { get; set; }
 
 
-            [Required(ErrorMessage = "主類別編號不可空白")]
+            [Required(ErrorMessage = "主類別不可空白")]
             [DisplayFormat(ConvertEmptyStringToNull = false, HtmlEncode = true, NullDisplayText = "請輸入主類別編號")]
-            [Display(Name = "主類別編號")]
+            [Display(Name = "主類別")]
             public Nullable<int> parentid { get; set; }
 
             [Required(ErrorMessage = "類別編號不可空白")]
@@ -29,7 +29,7 @@ namespace foodfun.Models
 
             [Required(ErrorMessage = "類別名稱不可空白")]
             [DisplayFormat(ConvertEmptyStringToNull = false, HtmlEncode = true, NullDisplayText = "請輸入類別名稱編號")]
-            [Display(Name = "類別名稱編號")]
+            [Display(Name = "類別名稱")]
             public string category_name { get; set; }
           
         }
