0811530fdabea6215540215cfc2730c5e5f38923
grace.tsai <gracetsai.3636@gmail.com>
2021-07-15 21:08:42 +0800

    about back

---
+++ foodfun/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,82 @@
+﻿using foodfun.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace foodfun.Areas.Admin.Controllers
+{
+    public class AboutController : Controller
+    {
+        GoPASTAEntities db = new GoPASTAEntities();
+        // GET: Admin/About
+        public ActionResult Index()
+        {
+            var data = db.About.OrderBy(m => m.rowid).ToList();
+
+            return View(data);
+
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            About model = new About();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(About model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            db.About.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+
+        public ActionResult Edit(int id)
+        {
+            var model = db.About.Where(m => m.sortid == id).FirstOrDefault();
+            return View(model);
+        }
+
+
+        [HttpPost]
+
+        public ActionResult Edit(About model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var data = db.About.Where(m => m.sortid == model.sortid).FirstOrDefault();
+            data.rowid = model.rowid;
+            data.corevalue_title = model.corevalue_title;
+            data.corevalue_descpt = model.corevalue_descpt;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+
+        public ActionResult Delete(int id)
+        {
+            using (GoPASTAEntities db = new GoPASTAEntities())
+            {
+                var model = db.About.Where(m => m.sortid == id).FirstOrDefault();
+                if (model != null)
+                {
+                    db.About.Remove(model);
+                    db.SaveChanges();
+                }
+                //return RedirectToAction("Index", "Product");    //Product為controller可略,會以目前controller為主
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
---
+++ foodfun/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,70 @@
+﻿using foodfun.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace foodfun.Areas.Admin.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Admin/Company
+        [LoginAuthorize(RoleList = "Admin")]
+        public ActionResult Index()
+        {
+            using (GoPASTAEntities db = new GoPASTAEntities())
+            {
+                Company model = db.Company.OrderBy(m => m.rowid).FirstOrDefault();
+
+                //if (model.rowid == 1)
+                //{
+                //    return View();
+                //}
+                //else
+                    return View(model);
+            }
+
+        }
+        
+
+        [HttpPost]
+
+        public ActionResult Index(Company model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            using (GoPASTAEntities db = new GoPASTAEntities())
+            {
+                var models = db.Company.Where(m => m.rowid == 1).FirstOrDefault();
+                models.company_id = model.company_id;
+                models.brandname = model.brandname;
+                models.tel = model.tel;
+                models.fax = model.fax;
+                models.address = model.address;
+                models.opentime = model.opentime;
+                models.closetime = model.closetime;
+                models.public_holiday = model.public_holiday;
+                models.description = model.description;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Upload( string product_no)
+        {
+            using (GoPASTAEntities db = new GoPASTAEntities())
+            {
+                var model = db.Products.Where(m => m.product_no == product_no).FirstOrDefault();
+
+                ImageService.ReturnAction("", "ProductBack", "Index");               
+                ImageService.ImageTitle = string.Format("{0}  圖片上傳", model.product_name);
+                ImageService.ImageFolder = "~/img/product";
+                ImageService.ImageSubFolder = model.category_no;
+                ImageService.ImageName = model.product_no; //給檔名
+                ImageService.ImageExtention = "jpg";
+                ImageService.UploadImageMode = true;
+                return RedirectToAction("UploadImage", "Image");
+            }
+        }
+    }
+}
